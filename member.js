7e6e7474df8b81ac0679cef27a950fe13a5c92e3
ynlzmn <80930840+ynlzmn@users.noreply.github.com>
2023-08-09 09:06:32 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  var skills = ["HTML", "CSS", "JS", "React", "Node"];
+  var member = {
+    name: "John",
+    age: 25,
+    skills: skills,
+  };
+  // Write code here
+  var memberSkills = member.skills; // Change this line
+  // Only change code above this line
+  return memberSkills;
+}
